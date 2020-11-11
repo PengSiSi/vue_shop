b8f1243167f6169d2a1f8b3c27e44144187ec260
PengSi <pengs@haidilao.com>
2020-11-11 17:25:50 +0800

    登录功能完成

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -3,8 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/global.css'
+import less from 'less'
+import {Form, FormItem, Input, Button, Select, Message} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(less)
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
 
 /* eslint-disable no-new */
 new Vue({
